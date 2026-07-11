22344e9c9073a20ab7d3394cd110244864dcf333
Apple <apple@192.168.1.8>
2026-07-11 20:02:21 +0500

    Fix typescript error in next config

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  // @ts-ignore
   eslint: {
     ignoreDuringBuilds: true,
   },
+  // @ts-ignore
   typescript: {
     ignoreBuildErrors: true,
   },
